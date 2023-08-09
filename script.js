8b541199f472ac6207b2189c94d41ebde746c605
Ola <hollabodey.kilaso@gmail.com>
2023-08-09 21:51:51 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let showBtn = document.getElementById('show-modal'),
 thankYou = document.querySelector('.thankYou'),
 overlay = document.querySelector('.overlay');
 
+let divIcon = document.querySelector('.divIcon'),
+BkmrkText = document.querySelector('.BkmrkText');
+
 
 showBtn.addEventListener('click', () => {
     thankYou.style.display = 'flex';
@@ -13,4 +16,9 @@ showBtn.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     thankYou.style.display = 'none';
     overlay.style.display = 'none';
+});
+
+
+divIcon.addEventListener('click', () => {
+    BkmrkText.innerText = 'Bookmarked';
 });
